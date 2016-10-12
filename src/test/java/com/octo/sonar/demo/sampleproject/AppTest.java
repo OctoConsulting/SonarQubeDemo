e23fadd03c10c382ec58f66afbc3ad5f2b4ef826
nvishy <vishy_work@yahoo.com>
2016-10-12 18:36:56 +0000

    test

--- src/test/java/com/octo/sonar/demo/sampleproject/AppTest.java
+++ src/test/java/com/octo/sonar/demo/sampleproject/AppTest.java
@@ -1,38 +1,72 @@
 package com.octo.sonar.demo.sampleproject;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
+	private static DemoCalculator calculator;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	@BeforeClass
+	public static void initCalculator() {
+		calculator = new DemoCalculator();
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-}
+	@Before
+	public void beforeEachTest() {
+		System.out.println("This is executed before each Test");
+	}
+
+	@After
+	public void afterEachTest() {
+		System.out.println("This is exceuted after each Test");
+	}
+
+	@Test
+	public void testSum() {
+		int result = calculator.findSum(3, 4);
+
+		assertEquals(7, result);
+	}
+
+	@Test
+	public void testDivison() {
+		try {
+			int result = calculator.findDivision(12, 2);
+
+			assertEquals(6, result);
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
+	}
+
+	@Test(expected = Exception.class)
+	public void testDivisionException() throws Exception {
+		calculator.findDivision(10, 0);
+	}
+
+	@Ignore
+	@Test
+	public void testEqual() {
+		boolean result = calculator.compareNumbers(20, 20);
+
+		assertFalse(result);
+	}
+
+	@Ignore
+	@Test
+	public void testSubstraction() {
+		int result = 10 - 3;
+
+		assertTrue(result == 9);
+	}
+	}
