e21f817d14660e6d5c9e68a84ba75df0de7b810f
nvishy <vishy_work@yahoo.com>
2017-01-20 10:19:32 -0500

    Commit
    Commit

--- src/main/java/com/octo/sonar/demo/sampleproject/DemoCalculator.java
+++ src/main/java/com/octo/sonar/demo/sampleproject/DemoCalculator.java
@@ -13,6 +13,7 @@ public class DemoCalculator implements DemoCalculatorInterface {
 	System.out.println ("The add sum is " + addSum);
 	System.out.println ("The add sum 2 is " + addSum);
 	System.out.println ("The add sum 3 is " + addSum);
+	System.out.println ("The add sum 3 is " + addSum);
 	/**
 	 * This is the demo calculator class
 	 *
