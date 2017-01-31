2a6c56fb3ab650017520779e5c59aabf6a9f6137
nvishy <vishy_work@yahoo.com>
2017-01-31 15:28:01 -0500

    test pipeline

--- src/main/java/com/octo/sonar/demo/sampleproject/App.java
+++ src/main/java/com/octo/sonar/demo/sampleproject/App.java
@@ -36,5 +36,6 @@ public class App
 		System.out.println ("Try SCM commit on Jenkins25");
 		System.out.println ("Try SCM commit on Jenkins26");
 		System.out.println ("Try SCM commit on Jenkins27");
+		System.out.println ("Try SCM commit on Jenkins28");
     }
 }
