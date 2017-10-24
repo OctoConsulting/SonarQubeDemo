4a1089a4af797e0525fd20fffad0cb065e3409af
nvishy <vishy_work@yahoo.com>
2017-10-23 22:15:34 -0400

    fix for SON-8
    fix for SON-8

--- src/main/java/com/octo/sonar/demo/sampleproject/App.java
+++ src/main/java/com/octo/sonar/demo/sampleproject/App.java
@@ -33,8 +33,6 @@ public class App
 		System.out.println ("Try SCM commit on Jenkins25");
 		System.out.println ("Try SCM commit on Jenkins28");
 		System.out.println ("Try SCM commit on Jenkins29");
-		System.out.println ("Try SCM commit on Jenkins27");
-		System.out.println ("Try SCM commit on Jenkins28");
 		System.out.println ("Try SCM commit on Jenkins29");
 		
 		System.out.println ("Try SCM commit on Jenkins28");
