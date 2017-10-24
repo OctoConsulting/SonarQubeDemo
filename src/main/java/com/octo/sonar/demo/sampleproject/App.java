19c8fb0288b28c364e90ae8a10d6401e21586773
nvishy <vishy_work@yahoo.com>
2017-10-23 22:28:57 -0400

    SON-12
    SON-12

--- src/main/java/com/octo/sonar/demo/sampleproject/App.java
+++ src/main/java/com/octo/sonar/demo/sampleproject/App.java
@@ -29,8 +29,6 @@ public class App
 		System.out.println ("Try SCM commit on Jenkins20");
 		System.out.println ("Try SCM commit on Jenkins22");
 		System.out.println ("Try SCM commit on Jenkins23");
-		System.out.println ("Try SCM commit on Jenkins24");
-		System.out.println ("Try SCM commit on Jenkins25");
 		System.out.println ("Try SCM commit on Jenkins28");
 		System.out.println ("Try SCM commit on Jenkins29");
 		System.out.println ("Try SCM commit on Jenkins29");
