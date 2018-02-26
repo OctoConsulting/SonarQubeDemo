79d129e1d6f69b0ef80d42a97cf8d32342345c54
nvishy <vishy_work@yahoo.com>
2018-02-26 14:17:38 -0500

    CALC-9

--- src/main/java/com/octo/sonar/demo/sampleproject/App.java
+++ src/main/java/com/octo/sonar/demo/sampleproject/App.java
@@ -11,8 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("Try SCM commit on Jenkins2");
         System.out.println ("Try SCM commit on Jenkins5");
-		System.out.println( "Hello World!" );
-		System.out.println( "Hello World!" );
         System.out.println("Try SCM commit on Jenkins2");
         System.out.println ("Try SCM commit on Jenkins5");
 		System.out.println ("Try SCM commit on Jenkins9");
