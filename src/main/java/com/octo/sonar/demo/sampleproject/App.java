622e6a5af3ddb4f7b68424bb9e370e98a9a3131f
nvishy <vishy_work@yahoo.com>
2019-05-08 12:45:12 -0400

    SON-13
    Fix message

--- src/main/java/com/octo/sonar/demo/sampleproject/App.java
+++ src/main/java/com/octo/sonar/demo/sampleproject/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
         System.out.println("Try SCM commit on Jenkins2");
         System.out.println ("Try SCM commit on Jenkins5");
         System.out.println("Try SCM commit on Jenkins2");
