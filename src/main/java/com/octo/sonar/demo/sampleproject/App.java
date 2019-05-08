16f38eee1e19081cf3437cb49a203cb7e16286fb
nvishy <vishy_work@yahoo.com>
2019-05-08 13:06:40 -0400

    SON-23
    Update log statements

--- src/main/java/com/octo/sonar/demo/sampleproject/App.java
+++ src/main/java/com/octo/sonar/demo/sampleproject/App.java
@@ -15,7 +15,6 @@ public class App
 		System.out.println ("Try SCM commit on Jenkins9");
 		System.out.println ("Try SCM commit on Jenkins10");
 		System.out.println ("Try SCM commit on Jenkins11");
-		System.out.println("Try SCM commit on Jenkins2");
         System.out.println ("Try SCM commit on Jenkins5");
 		System.out.println ("Try SCM commit on Jenkins9");
 		System.out.println ("Try SCM commit on Jenkins10");
